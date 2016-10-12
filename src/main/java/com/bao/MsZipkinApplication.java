082ce4a386f94e8b72c06ce37135edd3f81a2f02
baochunyu <baochunyu@nonobank.com>
2016-10-12 20:34:24 +0800

    init

---
+++ src/main/java/com/bao/MsZipkinApplication.java
@@ -0,0 +1,12 @@
+package com.bao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsZipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsZipkinApplication.class, args);
+	}
+}
