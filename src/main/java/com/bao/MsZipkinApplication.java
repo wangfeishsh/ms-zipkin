83d3091c41a30fa80bdd7cc2772c758e7518823c
baochunyu <baochunyu@nonobank.com>
2016-10-13 10:18:07 +0800

    init

--- src/main/java/com/bao/MsZipkinApplication.java
+++ src/main/java/com/bao/MsZipkinApplication.java
@@ -1,10 +1,33 @@
 package com.bao;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.sleuth.zipkin.ZipkinSpanReporter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @SpringBootApplication
 public class MsZipkinApplication {
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	// Use this for debugging (or if there is no Zipkin server running on port 9411)
+	@Bean
+	@ConditionalOnProperty(value = "sample.zipkin.enabled", havingValue = "false")
+	public ZipkinSpanReporter spanCollector() {
+		return new ZipkinSpanReporter() {
+			@Override
+			public void report(zipkin.Span span) {
+				log.info(String.format("Reporting span [%s]", span));
+			}
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsZipkinApplication.class, args);
