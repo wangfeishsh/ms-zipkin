c343ec0bbb990a0760ed7a9ecbcb2b3ec5dd38cc
baochunyu <baochunyu@nonobank.com>
2016-10-13 19:44:04 +0800

    init

--- src/main/java/com/bao/MsZipkinApplication.java
+++ src/main/java/com/bao/MsZipkinApplication.java
@@ -4,12 +4,23 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.cloud.sleuth.zipkin.ZipkinSpanReporter;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.http.HttpStatus;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Collection;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @SpringBootApplication
+//@EnableAspectJAutoProxy(proxyTargetClass = true)
+//@EnableAsync
 public class MsZipkinApplication {
 
 	@Bean
@@ -28,6 +39,23 @@ public class MsZipkinApplication {
 			}
 		};
 	}
+
+//	@Bean
+//	public EmbeddedServletContainerFactory servletContainer() {
+//		TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+//		factory.getContextValves();
+//		factory.setPort(9000);
+//		factory.setSessionTimeout(10, TimeUnit.MINUTES);
+//		factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/notfound.html"));
+//		factory.getContextValves();
+//		Collection collection = factory.getEngineValves();
+//		System.out.println("========");
+//		collection.forEach(v->{
+//			System.out.println(v.toString());
+//		});
+//		System.out.println("========");
+//		return factory;
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsZipkinApplication.class, args);
